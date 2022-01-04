42a6d05c50488994bc164da9749e2d4c70a1664f
Thaina <thaina2204@hotmail.com>
2022-01-04 15:30:53 -0300

    exercicios

--- Fundamentos/bloco-04-introduçao-a-javascript-e-logica-de-programaçao/dia-01-introduçao-a-javascript-e-logica-de-programaçao/scripts.js
+++ Fundamentos/bloco-04-introduçao-a-javascript-e-logica-de-programaçao/dia-01-introduçao-a-javascript-e-logica-de-programaçao/scripts.js
@@ -1,9 +1,10 @@
-const nota = 80;
+const currentHour = 22;
+var message = "";
 
-if (nota >= 80) {
-  console.log("Parabéns, você foi aprovado(a)!")
-} else if (nota < 80 && nota >= 60) {
-  console.log("Você está na nossa lista de espera.")
-} else {
-  console.log("Vocé foi reprovado(a).")
-}
+if (currentHour >= 22) {
+  message === "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+  message === "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+  message === "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour )
